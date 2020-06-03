5122467c51bea523d6e378dc8966a76474213f1f
Andreas Kruse <andreas.kruse@viadee.de>
2020-06-03 15:49:20 +0200

    Fix #50
    https://github.com/bpmn-io/moddle-xml/issues/50

--- test/spec/reader.js
+++ test/spec/reader.js
@@ -127,6 +127,27 @@ describe('Reader', function() {
         });
       });
 
+      it('simple / default xml ns', async function() {
+
+        // given
+        var reader = new Reader(model);
+        var rootHandler = reader.handler('props:ComplexAttrs');
+
+        var xml = '<props:complexAttrs xml:lang="en" xmlns:props="http://properties" ' +
+            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' +
+            '<props:attrs xsi:type="props:SubAttributes" integerValue="10" />' +
+            '</props:complexAttrs>';
+
+        // when
+        var {
+          rootElement, warnings
+        } = await reader.fromXML(xml, rootHandler);
+
+        // then
+        expect(warnings).to.be.empty;
+        expect(rootElement).not.to.be.empty;
+      });
+
 
       it('simple / xsi:type / default ns', async function() {
 
